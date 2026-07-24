abfaf2f9cfde2f70d56dea01a70008b25c59ed9d
Z User <z@container>
2026-07-24 05:07:47 +0000

    fix(android): bump compileSdk to 36 for file_picker dependency
    file_picker's transitive dependency flutter_plugin_android_lifecycle
    requires compileSdk 36+. Hardcode compileSdk=36 instead of using
    flutter.compileSdkVersion which resolves to 34.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.wisebreeze.brarchive"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
